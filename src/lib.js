0c2f42495fa06f81a02284fd9e8586161ed436fe
Kamen Raykov <pandarian1990@gmail.com>
2022-10-12 15:08:22 +0300

    Added project files

---
+++ src/lib.js
@@ -0,0 +1,13 @@
+import { html, render } from './lib/lit-html.js';
+import { until } from './lib/directives/until.js';
+import { classMap } from './lib/directives/class-map.js';
+import page from './lib/page.mjs';
+
+
+export {
+    html,
+    render,
+    until,
+    classMap,
+    page
+};
